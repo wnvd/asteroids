a2587e6dcb3cd37a25f8fbc23e7c04f6e7b024ba
wnvd <waninaveed998@gmail.com>
2024-12-28 14:34:10 +0530

    lock the game to 60 fps

--- main.py
+++ main.py
@@ -11,13 +11,17 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("green") 
+        screen.fill("black") 
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
