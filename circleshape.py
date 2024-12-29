30bd24c07fa5f817fd3c13577be66e5bad2b70e2
wnvd <waninaveed998@gmail.com>
2024-12-29 13:48:35 +0530

    add a triangle that can rotate

---
+++ circleshape.py
@@ -0,0 +1,20 @@
+import pygame
+
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+    
+
+    def draw():     
+        pass
+
+
+    def update():
+        pass
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import *
 
 def main():
     pygame.init()
@@ -14,12 +14,16 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(x= SCREEN_WIDTH / 2, y= SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black") 
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
